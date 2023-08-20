43c57202de1ba06fc9ae57d3d6932c22376ce925
joao4xz <joao.4xz@gmail.com>
2023-08-20 20:13:24 -0300

    Add 3 day forecast

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,6 +1,9 @@
-import fetchCity from './fetch';
+import { fetchCurrentCity, fetchForecastCity } from './fetch';
 
-let cityObject;
+const { format, parse } = require('date-fns');
+
+let currentCityObject;
+let forecastCityObject;
 
 function changeCurrentCity() {
   const cityName = document.getElementById('current-city');
@@ -8,17 +11,87 @@ function changeCurrentCity() {
   const phrase = document.getElementById('current-phrase');
   const image = document.getElementById('current-icon');
 
-  cityName.textContent = cityObject.location.name;
+  cityName.textContent = currentCityObject.location.name;
 
   if (document.getElementById('cf-button').textContent === 'C') {
-    tempText.textContent = `${cityObject.current.temp_c}°C`;
+    tempText.textContent = `${currentCityObject.current.temp_c}°C`;
   } else {
-    tempText.textContent = `${cityObject.current.temp_f}°F`;
+    tempText.textContent = `${currentCityObject.current.temp_f}°F`;
   }
 
-  phrase.textContent = cityObject.current.condition.text;
+  phrase.textContent = currentCityObject.current.condition.text;
 
-  image.src = cityObject.current.condition.icon;
+  image.src = currentCityObject.current.condition.icon;
+}
+
+function changeForecastCities() {
+  const todayCityName = document.getElementById('today-city');
+  const todayTempText = document.getElementById('today-temp');
+  const todayPhrase = document.getElementById('today-phrase');
+  const todayImage = document.getElementById('today-icon');
+  const todayDate = document.getElementById('today-date');
+
+  const tomorrowCityName = document.getElementById('tomorrow-city');
+  const tomorrowTempText = document.getElementById('tomorrow-temp');
+  const tomorrowPhrase = document.getElementById('tomorrow-phrase');
+  const tomorrowImage = document.getElementById('tomorrow-icon');
+  const tomorrowDate = document.getElementById('tomorrow-date');
+
+  const afterCityName = document.getElementById('after-city');
+  const afterTempText = document.getElementById('after-temp');
+  const afterPhrase = document.getElementById('after-phrase');
+  const afterImage = document.getElementById('after-icon');
+  const afterDate = document.getElementById('after-date');
+
+  const cityName = forecastCityObject.location.name;
+
+  todayCityName.textContent = cityName;
+  tomorrowCityName.textContent = cityName;
+  afterCityName.textContent = cityName;
+
+  todayPhrase.textContent =
+    forecastCityObject.forecast.forecastday[0].day.condition.text;
+  tomorrowPhrase.textContent =
+    forecastCityObject.forecast.forecastday[1].day.condition.text;
+  afterPhrase.textContent =
+    forecastCityObject.forecast.forecastday[2].day.condition.text;
+
+  todayImage.src =
+    forecastCityObject.forecast.forecastday[0].day.condition.icon;
+  tomorrowImage.src =
+    forecastCityObject.forecast.forecastday[1].day.condition.icon;
+  afterImage.src =
+    forecastCityObject.forecast.forecastday[2].day.condition.icon;
+
+  if (document.getElementById('cf-button').textContent === 'C') {
+    todayTempText.textContent = `${forecastCityObject.forecast.forecastday[0].day.avgtemp_c}°C`;
+    tomorrowTempText.textContent = `${forecastCityObject.forecast.forecastday[1].day.avgtemp_c}°C`;
+    afterTempText.textContent = `${forecastCityObject.forecast.forecastday[2].day.avgtemp_c}°C`;
+  } else {
+    todayTempText.textContent = `${forecastCityObject.forecast.forecastday[0].day.avgtemp_f}°F`;
+    tomorrowTempText.textContent = `${forecastCityObject.forecast.forecastday[1].day.avgtemp_f}°F`;
+    afterTempText.textContent = `${forecastCityObject.forecast.forecastday[2].day.avgtemp_f}°F`;
+  }
+
+  const parsedTodayDate = parse(
+    forecastCityObject.forecast.forecastday[0].date,
+    'yyyy-MM-dd',
+    new Date()
+  );
+  const parsedTomorrowDate = parse(
+    forecastCityObject.forecast.forecastday[1].date,
+    'yyyy-MM-dd',
+    new Date()
+  );
+  const parsedAfterDate = parse(
+    forecastCityObject.forecast.forecastday[2].date,
+    'yyyy-MM-dd',
+    new Date()
+  );
+
+  todayDate.textContent = format(parsedTodayDate, 'MMM dd');
+  tomorrowDate.textContent = format(parsedTomorrowDate, 'MMM dd');
+  afterDate.textContent = format(parsedAfterDate, 'MMM dd');
 }
 
 function handleInvalidCity() {
@@ -30,15 +103,24 @@ function handleInvalidCity() {
 }
 
 function convertTemperature() {
-  const tempText = document.getElementById('current-temp');
   const tempButton = document.getElementById('cf-button');
-  console.log('Hey');
+  const tempText = document.getElementById('current-temp');
+
+  const todayTemp = document.getElementById('today-temp');
+  const tomorrowTemp = document.getElementById('tomorrow-temp');
+  const afterTemp = document.getElementById('after-temp');
 
   if (tempButton.textContent === 'C') {
-    tempText.textContent = `${cityObject.current.temp_f}°F`;
+    tempText.textContent = `${currentCityObject.current.temp_f}°F`;
+    todayTemp.textContent = `${forecastCityObject.forecast.forecastday[0].day.avgtemp_f}°F`;
+    tomorrowTemp.textContent = `${forecastCityObject.forecast.forecastday[1].day.avgtemp_f}°F`;
+    afterTemp.textContent = `${forecastCityObject.forecast.forecastday[2].day.avgtemp_f}°F`;
     tempButton.textContent = 'F';
   } else {
-    tempText.textContent = `${cityObject.current.temp_c}°C`;
+    tempText.textContent = `${currentCityObject.current.temp_c}°C`;
+    todayTemp.textContent = `${forecastCityObject.forecast.forecastday[0].day.avgtemp_c}°C`;
+    tomorrowTemp.textContent = `${forecastCityObject.forecast.forecastday[1].day.avgtemp_c}°C`;
+    afterTemp.textContent = `${forecastCityObject.forecast.forecastday[2].day.avgtemp_c}°C`;
     tempButton.textContent = 'C';
   }
 }
@@ -52,9 +134,12 @@ function convertListener() {
 
 export async function searchCity(cityName) {
   try {
-    cityObject = await fetchCity(cityName);
+    currentCityObject = await fetchCurrentCity(cityName);
+    forecastCityObject = await fetchForecastCity(cityName);
     changeCurrentCity();
-    console.log(cityObject);
+    changeForecastCities();
+    console.log(currentCityObject);
+    console.log(forecastCityObject);
   } catch (error) {
     handleInvalidCity();
   }
--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,10 +1,22 @@
-export default async function fetchCity(city) {
+export async function fetchCurrentCity(city) {
   try {
     const response = await fetch(
       `https://api.weatherapi.com/v1/current.json?key=a546a5e4168344e08a0184328231408&q=${city}`
     );
     const responseJSON = await response.json();
-    console.log('Completed!');
+    return responseJSON;
+  } catch (e) {
+    throw new Error(e);
+  }
+}
+
+export async function fetchForecastCity(city) {
+  try {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=a546a5e4168344e08a0184328231408&q=${city}&days=3
+      `
+    );
+    const responseJSON = await response.json();
     return responseJSON;
   } catch (e) {
     throw new Error(e);
