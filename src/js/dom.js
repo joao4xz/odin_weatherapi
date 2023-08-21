73f149d7a32f58fd0a4ff4d8b6087a720c47f569
joao4xz <joao.4xz@gmail.com>
2023-08-20 22:05:51 -0300

    Add 5 day history

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,9 +1,10 @@
-import { fetchCurrentCity, fetchForecastCity } from './fetch';
+import { fetchCurrentCity, fetchForecastCity, fetchHistoryCity } from './fetch';
 
-const { format, parse } = require('date-fns');
+const { format, parse, subDays } = require('date-fns');
 
 let currentCityObject;
 let forecastCityObject;
+let historyCityArray;
 
 function changeCurrentCity() {
   const cityName = document.getElementById('current-city');
@@ -94,6 +95,114 @@ function changeForecastCities() {
   afterDate.textContent = format(parsedAfterDate, 'MMM dd');
 }
 
+function changeHistoryCities() {
+  const firstCityName = document.getElementById('first-city');
+  const firstTempText = document.getElementById('first-temp');
+  const firstPhrase = document.getElementById('first-phrase');
+  const firstImage = document.getElementById('first-icon');
+  const firstDate = document.getElementById('first-date');
+
+  const secondCityName = document.getElementById('second-city');
+  const secondTempText = document.getElementById('second-temp');
+  const secondPhrase = document.getElementById('second-phrase');
+  const secondImage = document.getElementById('second-icon');
+  const secondDate = document.getElementById('second-date');
+
+  const thirdCityName = document.getElementById('third-city');
+  const thirdTempText = document.getElementById('third-temp');
+  const thirdPhrase = document.getElementById('third-phrase');
+  const thirdImage = document.getElementById('third-icon');
+  const thirdDate = document.getElementById('third-date');
+
+  const fourthCityName = document.getElementById('fourth-city');
+  const fourthTempText = document.getElementById('fourth-temp');
+  const fourthPhrase = document.getElementById('fourth-phrase');
+  const fourthImage = document.getElementById('fourth-icon');
+  const fourthDate = document.getElementById('fourth-date');
+
+  const fifthCityName = document.getElementById('fifth-city');
+  const fifthTempText = document.getElementById('fifth-temp');
+  const fifthPhrase = document.getElementById('fifth-phrase');
+  const fifthImage = document.getElementById('fifth-icon');
+  const fifthDate = document.getElementById('fifth-date');
+
+  const cityName = historyCityArray[0].location.name;
+
+  firstCityName.textContent = cityName;
+  secondCityName.textContent = cityName;
+  thirdCityName.textContent = cityName;
+  fourthCityName.textContent = cityName;
+  fifthCityName.textContent = cityName;
+
+  firstPhrase.textContent =
+    historyCityArray[4].forecast.forecastday[0].day.condition.text;
+  secondPhrase.textContent =
+    historyCityArray[3].forecast.forecastday[0].day.condition.text;
+  thirdPhrase.textContent =
+    historyCityArray[2].forecast.forecastday[0].day.condition.text;
+  fourthPhrase.textContent =
+    historyCityArray[1].forecast.forecastday[0].day.condition.text;
+  fifthPhrase.textContent =
+    historyCityArray[0].forecast.forecastday[0].day.condition.text;
+
+  firstImage.src =
+    historyCityArray[4].forecast.forecastday[0].day.condition.icon;
+  secondImage.src =
+    historyCityArray[3].forecast.forecastday[0].day.condition.icon;
+  thirdImage.src =
+    historyCityArray[2].forecast.forecastday[0].day.condition.icon;
+  fourthImage.src =
+    historyCityArray[1].forecast.forecastday[0].day.condition.icon;
+  fifthImage.src =
+    historyCityArray[0].forecast.forecastday[0].day.condition.icon;
+
+  if (document.getElementById('cf-button').textContent === 'C') {
+    firstTempText.textContent = `${historyCityArray[4].forecast.forecastday[0].day.avgtemp_c}°C`;
+    secondTempText.textContent = `${historyCityArray[3].forecast.forecastday[0].day.avgtemp_c}°C`;
+    thirdTempText.textContent = `${historyCityArray[2].forecast.forecastday[0].day.avgtemp_c}°C`;
+    fourthTempText.textContent = `${historyCityArray[1].forecast.forecastday[0].day.avgtemp_c}°C`;
+    fifthTempText.textContent = `${historyCityArray[0].forecast.forecastday[0].day.avgtemp_c}°C`;
+  } else {
+    firstTempText.textContent = `${historyCityArray[4].forecast.forecastday[0].day.avgtemp_f}°F`;
+    secondTempText.textContent = `${historyCityArray[3].forecast.forecastday[0].day.avgtemp_f}°F`;
+    thirdTempText.textContent = `${historyCityArray[2].forecast.forecastday[0].day.avgtemp_f}°F`;
+    fourthTempText.textContent = `${historyCityArray[1].forecast.forecastday[0].day.avgtemp_f}°F`;
+    fifthTempText.textContent = `${historyCityArray[0].forecast.forecastday[0].day.avgtemp_f}°F`;
+  }
+
+  const parsedFirstDate = parse(
+    historyCityArray[4].forecast.forecastday[0].date,
+    'yyyy-MM-dd',
+    new Date()
+  );
+  const parsedSecondDate = parse(
+    historyCityArray[3].forecast.forecastday[0].date,
+    'yyyy-MM-dd',
+    new Date()
+  );
+  const parsedThirdDate = parse(
+    historyCityArray[2].forecast.forecastday[0].date,
+    'yyyy-MM-dd',
+    new Date()
+  );
+  const parsedFourthDate = parse(
+    historyCityArray[1].forecast.forecastday[0].date,
+    'yyyy-MM-dd',
+    new Date()
+  );
+  const parsedFifthDate = parse(
+    historyCityArray[0].forecast.forecastday[0].date,
+    'yyyy-MM-dd',
+    new Date()
+  );
+
+  firstDate.textContent = format(parsedFirstDate, 'MMM dd');
+  secondDate.textContent = format(parsedSecondDate, 'MMM dd');
+  thirdDate.textContent = format(parsedThirdDate, 'MMM dd');
+  fourthDate.textContent = format(parsedFourthDate, 'MMM dd');
+  fifthDate.textContent = format(parsedFifthDate, 'MMM dd');
+}
+
 function handleInvalidCity() {
   const input = document.getElementById('search-input');
   input.classList.add('error');
@@ -110,17 +219,33 @@ function convertTemperature() {
   const tomorrowTemp = document.getElementById('tomorrow-temp');
   const afterTemp = document.getElementById('after-temp');
 
+  const firstTemp = document.getElementById('first-temp');
+  const secondTemp = document.getElementById('second-temp');
+  const thirdTemp = document.getElementById('third-temp');
+  const fourthTemp = document.getElementById('fourth-temp');
+  const fifthTemp = document.getElementById('fifth-temp');
+
   if (tempButton.textContent === 'C') {
     tempText.textContent = `${currentCityObject.current.temp_f}°F`;
     todayTemp.textContent = `${forecastCityObject.forecast.forecastday[0].day.avgtemp_f}°F`;
     tomorrowTemp.textContent = `${forecastCityObject.forecast.forecastday[1].day.avgtemp_f}°F`;
     afterTemp.textContent = `${forecastCityObject.forecast.forecastday[2].day.avgtemp_f}°F`;
+    firstTemp.textContent = `${historyCityArray[4].forecast.forecastday[0].day.avgtemp_f}°F`;
+    secondTemp.textContent = `${historyCityArray[3].forecast.forecastday[0].day.avgtemp_f}°F`;
+    thirdTemp.textContent = `${historyCityArray[2].forecast.forecastday[0].day.avgtemp_f}°F`;
+    fourthTemp.textContent = `${historyCityArray[1].forecast.forecastday[0].day.avgtemp_f}°F`;
+    fifthTemp.textContent = `${historyCityArray[0].forecast.forecastday[0].day.avgtemp_f}°F`;
     tempButton.textContent = 'F';
   } else {
     tempText.textContent = `${currentCityObject.current.temp_c}°C`;
     todayTemp.textContent = `${forecastCityObject.forecast.forecastday[0].day.avgtemp_c}°C`;
     tomorrowTemp.textContent = `${forecastCityObject.forecast.forecastday[1].day.avgtemp_c}°C`;
     afterTemp.textContent = `${forecastCityObject.forecast.forecastday[2].day.avgtemp_c}°C`;
+    firstTemp.textContent = `${historyCityArray[4].forecast.forecastday[0].day.avgtemp_c}°C`;
+    secondTemp.textContent = `${historyCityArray[3].forecast.forecastday[0].day.avgtemp_c}°C`;
+    thirdTemp.textContent = `${historyCityArray[2].forecast.forecastday[0].day.avgtemp_c}°C`;
+    fourthTemp.textContent = `${historyCityArray[1].forecast.forecastday[0].day.avgtemp_c}°C`;
+    fifthTemp.textContent = `${historyCityArray[0].forecast.forecastday[0].day.avgtemp_c}°C`;
     tempButton.textContent = 'C';
   }
 }
@@ -132,14 +257,31 @@ function convertListener() {
   });
 }
 
+function getLastDays() {
+  const today = new Date();
+
+  const previousDays = [];
+
+  for (let i = 1; i <= 5; i++) {
+    const previousDate = subDays(today, i);
+    const formattedDate = format(previousDate, 'yyyy-MM-dd');
+    previousDays.push(formattedDate);
+  }
+  return previousDays;
+}
+
 export async function searchCity(cityName) {
   try {
     currentCityObject = await fetchCurrentCity(cityName);
     forecastCityObject = await fetchForecastCity(cityName);
+    const lastDays = getLastDays();
+    historyCityArray = await fetchHistoryCity(cityName, lastDays);
     changeCurrentCity();
     changeForecastCities();
+    changeHistoryCities();
     console.log(currentCityObject);
     console.log(forecastCityObject);
+    console.log(historyCityArray);
   } catch (error) {
     handleInvalidCity();
   }
--- src/js/fetch.js
+++ src/js/fetch.js
@@ -22,3 +22,24 @@ export async function fetchForecastCity(city) {
     throw new Error(e);
   }
 }
+
+export async function fetchHistoryCity(city, date) {
+  try {
+    const history = [];
+    for (let i = 0; i < 5; i++) {
+      const response = fetch(
+        `https://api.weatherapi.com/v1/history.json?key=a546a5e4168344e08a0184328231408&q=${city}&dt=${date[i]}
+        `
+      );
+      history.push(response);
+    }
+
+    const responses = await Promise.all(history);
+    const responsesJSONs = await Promise.all(
+      responses.map((response) => response.json())
+    );
+    return responsesJSONs;
+  } catch (e) {
+    throw new Error(e);
+  }
+}
